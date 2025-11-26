dbe0cd83458e305680e44c7263dcebe38150bce5
Inmaculada EIRL <hf46078@gmail.com>
2025-11-26 08:30:05 -0500

    La jueee

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,64 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as path from 'path';
-import * as fs from 'fs';
+import serverless from 'serverless-http';
+import express from 'express';
+
+const expressApp = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    // Opciones de Nest
+    logger: ['error', 'warn', 'log'],
+  });
+  
+  app.enableCors();
+  await app.init();
 
-  // Ruta al modelo en dist/assets
-  // const modelPath = path.join(__dirname, 'assets', 'bria.onnx');
+  // Monta NestJS sobre Express
+  app.getHttpAdapter().getInstance() // devuelve Express interno de Nest
+    .use(expressApp); // opcional, si quieres middleware adicional
 
-  // try {
-  //   const stats = fs.statSync(modelPath);
-  //   console.log(`Archivo encontrado: ${modelPath}`);
-  //   console.log(`Tamaño: ${stats.size} bytes`);
-  // } catch (err) {
-  //   console.error(`❌ No se pudo leer el archivo: ${modelPath}`);
-  // }
-
-  await app.listen(process.env.PORT ?? 3000);
-  console.log("API on http://localhost:3000");
-  console.log("WS  on ws://localhost:3000/ws");
+  console.log("NestJS listo para serverless");
 }
-//
-bootstrap();
+
+// Localmente
+if (process.env.NODE_ENV !== 'production') {
+  bootstrap().then(() => {
+    expressApp.listen(3000, () => {
+      console.log("API on http://localhost:3000");
+    });
+  });
+}
+
+// Exporta handler para Vercel
+export const handler = serverless(expressApp);
+
+
+
+
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import serverless from 'serverless-http';
+
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   Ruta al modelo en dist/assets
+//   const modelPath = path.join(__dirname, 'assets', 'bria.onnx');
+
+//   try {
+//     const stats = fs.statSync(modelPath);
+//     console.log(`Archivo encontrado: ${modelPath}`);
+//     console.log(`Tamaño: ${stats.size} bytes`);
+//   } catch (err) {
+//     console.error(`❌ No se pudo leer el archivo: ${modelPath}`);
+//   }
+
+//   await app.listen(process.env.PORT ?? 3000);
+//   console.log("API on http://localhost:3000");
+//   console.log("WS  on ws://localhost:3000/ws");
+// }
+// //
+// bootstrap();
